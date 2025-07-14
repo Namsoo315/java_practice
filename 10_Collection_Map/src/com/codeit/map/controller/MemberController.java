44ce342d9e2c34488e787b992c18e281a5710161
Namsoo315 <gustn2130@naver.com>
2025-07-14 15:12:21 +0900

    feat : 10_Collection_Map practice add

--- 10_Collection_Map/src/com/codeit/map/controller/MemberController.java
+++ 10_Collection_Map/src/com/codeit/map/controller/MemberController.java
@@ -8,34 +8,53 @@ import com.codeit.map.model.Member;
 
 public class MemberController {
 	private final Map<String, Member> map = new HashMap<>();
-	
+
 	public MemberController() {
-		map.put("test1", new Member("1234", "홍길동"));
-		map.put("test2", new Member("1234", "홍길동"));
-		map.put("test3", new Member("1234", "홍길동"));
-		map.put("test4", new Member("1234", "홍길동"));
-		map.put("test5", new Member("1234", "홍길동"));
+
 	}
 
 	public boolean joinMembership(String id, Member m) {
-		// 여기 채우세요.
+		if (map.containsKey(id)) {
+			return false;
+		}
+
+		map.put(id, m);
+		return true;
 	}
 
 	public String login(String id, String password) {
-		// 여기 채우세요.
+		if (map.containsKey(id)) {
+			if (password.equals(map.get(id).getPassword())) {
+				return map.get(id).getName();
+			}
+		}
+		return null;
 	}
 
 	public boolean changePassword(String id, String oldPw, String newPw) {
-		// 여기 채우세요.
+		if (map.containsKey(id)) {
+			if (map.get(id).getPassword().equals(oldPw)) {
+				map.get(id).setPassword(newPw);
+				return true;
+			}
+		}
+		return false;
 	}
 
-	public boolean changeName(String id, String newName) {
-		// 여기 채우세요.
+	public void changeName(String id, String newName) {
+		map.get(id).setName(newName);
 	}
 
 	public TreeMap<String, Member> sameName(String name) {
-		// 여기 채우세요.
+		TreeMap<String, Member> result = new TreeMap<>();
+		for (String key : map.keySet()) {
+			Member m = map.get(key);
+			if (m.getName().equals(name)) {
+				result.put(key, m);
+			}
+		}
 
+		return result;
 	}
 }
 
--- 10_Collection_Map/src/com/codeit/map/model/Member.java
+++ 10_Collection_Map/src/com/codeit/map/model/Member.java
@@ -1,6 +1,50 @@
 package com.codeit.map.model;
 
 public class Member {
+	private String id;
+	private String password;
+	private String name;
 
+	public Member(){
 
+	}
+
+	public Member(String password, String name) {
+		this.password = password;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuffer sb = new StringBuffer("Member{");
+		sb.append("id='").append(id).append('\'');
+		sb.append(", password='").append(password).append('\'');
+		sb.append(", name='").append(name).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
 }
