386b18a9ab788e554e7d1a690c80ec4294267a36
Namsoo315 <gustn2130@naver.com>
2025-09-17 11:43:11 +0900

    feat: RestControllerAdvice 실습

--- 17_spring_blog_backend_querydsl/src/main/java/com/codeit/blog/repository/impl/PostQueryRepositoryImpl.java
+++ 17_spring_blog_backend_querydsl/src/main/java/com/codeit/blog/repository/impl/PostQueryRepositoryImpl.java
@@ -1,22 +1,25 @@
 package com.codeit.blog.repository.impl;
 
 import com.codeit.blog.dto.post.PostFlatDto;
-import com.codeit.blog.entity.*;
+import com.codeit.blog.entity.Category;
+import com.codeit.blog.entity.Post;
+import com.codeit.blog.entity.QBinaryContent;
+import com.codeit.blog.entity.QComment;
+import com.codeit.blog.entity.QPost;
+import com.codeit.blog.entity.QUser;
 import com.codeit.blog.repository.PostQueryRepository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.expression.spel.ast.Projection;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
---
+++ 18_spring_exception/src/main/java/com/codeit/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.codeit.blog.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    log.error("예상치 못한 오류 발생 : {}", e.getMessage(), e);
+    ErrorResponse errorResponse = new ErrorResponse(e, 500);
+    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+  }
+
+  // 커스텀 에러 처리
+  @ExceptionHandler(BaseException.class)
+  public ResponseEntity<ErrorResponse> handleCustomException(BaseException e) {
+    log.error("커스텀 예외 발생 : code={}, message : {}", e.getErrorCode(), e.getMessage(), e);
+    ErrorResponse errorResponse = new ErrorResponse(e, HttpStatus.BAD_REQUEST.value());
+    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+  }
+
+  private HttpStatus determineHttpStatus(BaseException exception) {
+    ErrorCode errorCode = exception.getErrorCode();
+    return switch (errorCode) {
+      case USER_NOT_FOUND -> HttpStatus.NOT_FOUND;
+      case DUPLICATE_USER -> HttpStatus.CONFLICT;
+      case INVALID_USER_CREDENTIALS -> HttpStatus.UNAUTHORIZED;
+      case INVALID_USER_PARAMETER, INVALID_REQUEST -> HttpStatus.BAD_REQUEST;
+      case INTERNAL_SERVER_ERROR -> HttpStatus.INTERNAL_SERVER_ERROR;
+    };
+  }
+}
